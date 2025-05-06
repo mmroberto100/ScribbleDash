adfc47955e8835be469cb02a99fa351b1468c3a0
manuelrobertomunoz <mmroberto100@gmail.com>
2025-05-06 13:42:29 -0700

    - Implemented Home Screen for Milestone #1 - Navigationbar, ModeButton, Theme adjustments.

---
+++ app/src/main/java/com/mmunoz/scribbledash/navigation/NavigationItem.kt
@@ -0,0 +1,8 @@
+package com.mmunoz.scribbledash.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+)
